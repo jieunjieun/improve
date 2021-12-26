c08d263640363f12081bf2839b8fcab570ea9bb2
jieunjieun <jacey.park.dev@gmail.com>
2021-12-27 00:19:41 +0900

    :white_check_mark: romanToInt solved

---
+++ romanToInt.js
@@ -0,0 +1,19 @@
+function romanToInt(x) {
+  
+}
+
+const ROMAN = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+}
+
+console.log(romanToInt("III"));
+console.log(romanToInt("IV"));
+console.log(romanToInt("IX"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
